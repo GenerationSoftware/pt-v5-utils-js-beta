b687359f68dd9ae2d8259a26643472741c359a92
Charles Bergeron <chuck.bergeron@gmail.com>
2023-08-31 13:38:25 -0700

    add mainnet beta subgraphs and publish beta 41

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 const CHAIN_ID = {
   goerli: 5,
+  optimism: 10,
   optimismGoerli: 420,
 };
 
@@ -8,15 +9,19 @@ interface StringMap {
 }
 
 export const CONTRACTS_STORE: StringMap = {
+  '1': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/main/deployments/ethereum/contracts.json',
   '5': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/main/deployments/ethGoerli/contracts.json',
+  '10': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/main/deployments/optimism/contracts.json',
   '420':
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/main/deployments/optimismGoerli/contracts.json',
 };
 
 export const TWAB_CONTROLLER_SUBGRAPH_URIS = {
+  [CHAIN_ID.optimism]: `https://api.studio.thegraph.com/query/41211/pt-v5-optimism-twab-controller/v0.0.1`,
   [CHAIN_ID.optimismGoerli]: `https://api.studio.thegraph.com/query/41211/pt-v5-op-goerli-twab-control/v0.0.2`,
 };
 
 export const PRIZE_POOL_SUBGRAPH_URIS = {
+  [CHAIN_ID.optimism]: `https://api.studio.thegraph.com/query/41211/pt-v5-optimism-prize-pool/v0.0.1`,
   [CHAIN_ID.optimismGoerli]: `https://api.studio.thegraph.com/query/41211/pt-v5-op-goerli-prize-pool/v0.0.2`,
 };
